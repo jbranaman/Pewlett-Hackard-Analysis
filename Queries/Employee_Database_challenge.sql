02f6a4ec51a5094dedc4d593bea34c49b9bf39cd
jbranaman <96451672+jbranaman@users.noreply.github.com>
2022-02-18 12:55:09 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -40,4 +40,5 @@ ON (e.emp_no = de.emp_no)
 Left outer Join titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
